00ddde0d3939bd4fce13a2b6cc732d2a50a387d9
Zachary Segall <segallza@grinnell.edu>
2017-03-16 08:11:01 -0500

    Add ctors tests

--- Zachary/apilib.h
+++ Zachary/apilib.h
@@ -16,6 +16,15 @@ typedef struct ap_int {
   int stub;
 } APInt;
 
+// +-----------+---------------------------------------------
+// | Constants |
+// +-----------+
+
+APInt API_ZERO;
+APInt API_ONE;
+APInt API_TWO;
+APInt API_NEG_ONE;
+
 // +-------------------+---------------------------------------------
 // | Library Functions |
 // +-------------------+
@@ -31,47 +40,6 @@ void api_init();
  * is exited.
  */
 void api_cleanup();
-
-// +-----------+-----------------------------------------------------
-// | Operators |
-// +-----------+
-
-/**
- * Add two arbitrarily large integers, creating a newly allocated
- * arbitrarily large integer.  The client is responsible for freeing
- * any memory associated with the new value using ali_free.
- */
-APInt * api_add (APInt *a, APInt *b);
-
-/**
- * Subtract two arbitrarily large integers, creating a newly allocated
- * arbitrarily large integer.  The client is responsible for freeing
- * any memory associated with the new value using ali_free.
- */
-APInt * api_subtract (APInt *a, APInt *b);
-
-/**
- * Multiply two arbitrarily large integers, creating a newly allocated
- * arbitrarily large integer.  The client is responsible for freeing
- * any memory associated with the new value using ali_free.
- */
-APInt * api_product (APInt *a, APInt *b);
-
-
-/**
- * Find the remainder of two arbitrarily large integers, creating a 
- * newly allocated arbitrarily large integer.  The client is responsible 
- * for freeing any memory associated with the new value using ali_free.
- */
-APInt * api_remainder (APInt *a, APInt *b);
-
-/**
- * Find the quotient of two arbitrarily large integers, creating a 
- * newly allocated arbitrarily large integer.  The client is responsible 
- * for freeing any memory associated with the new value using ali_free.
- */
-APInt * api_quotient (APInt *a, APInt *b);
-
 
 // +--------------+---------------------------------------------------
 // | Constructors |
@@ -127,6 +95,52 @@ long api2long (APInt * api);
   */
 double api2double (APInt * api);
 
+// +-----------+-----------------------------------------------------
+// | Operators |
+// +-----------+
+
+/**
+ * Add two arbitrarily large integers, creating a newly allocated
+ * arbitrarily large integer.  The client is responsible for freeing
+ * any memory associated with the new value using ali_free.
+ */
+APInt * api_add (APInt *a, APInt *b);
+
+/**
+ * Subtract two arbitrarily large integers, creating a newly allocated
+ * arbitrarily large integer.  The client is responsible for freeing
+ * any memory associated with the new value using ali_free.
+ */
+APInt * api_subtract (APInt *a, APInt *b);
+
+/**
+ * Multiply two arbitrarily large integers, creating a newly allocated
+ * arbitrarily large integer.  The client is responsible for freeing
+ * any memory associated with the new value using ali_free.
+ */
+APInt * api_product (APInt *a, APInt *b);
+
+
+/**
+ * Find the remainder of two arbitrarily large integers, creating a 
+ * newly allocated arbitrarily large integer.  The client is responsible 
+ * for freeing any memory associated with the new value using ali_free.
+ */
+APInt * api_remainder (APInt *a, APInt *b);
+
+/**
+ * Find the quotient of two arbitrarily large integers, creating a 
+ * newly allocated arbitrarily large integer.  The client is responsible 
+ * for freeing any memory associated with the new value using ali_free.
+ */
+APInt * api_quotient (APInt *a, APInt *b);
+
+/**
+ * Compare two API values. Return -1 if the first is less than the 
+ * second, 0 if they are the same, and 1 if the first is greater than 
+ * the second
+ */
+int * api_cmp (APInt *a, APInt *b);
 
 
 #endif // __APILIB_H_
---
+++ Zachary/test-api-ctors.c
@@ -0,0 +1,61 @@
+/**
+ * test-api-ctors.c
+ *   Tests for the constructors for the APInt library.
+ *
+ * <Insert appropriate open-source license.>
+ */
+
+// +---------+-------------------------------------------------------
+// | Headers |
+// +---------+
+
+#include <assert.h>
+#include <limits.h>
+#include "apilib.h"
+
+// +------+----------------------------------------------------------
+// | Main |
+// +------+
+
+int
+main (int argc, char *argv[<a href="#referent" id="reference"></a>])
+{
+
+  int    large = 10000;
+  char*  large_str = "10000";
+  long   large_long = 72036854775808;
+  char*  large_long_str = "72036854775808";
+  int    nlarge = -10000;
+  char*  nlarge_str = "-10000";
+  long   nlarge_long = -72036854775808;
+  char*  nlarge_long_str = "-72036854775808";
+  
+  // Constants
+  assert (api_cmp(API_ZERO, int2api(0)) == 0);
+  assert (api_cmp(API_ZERO, str2api("0")) == 0);
+  assert (api_cmp(API_ZERO, long2api((long) 0)) == 0);
+  assert (api_cmp(API_ZERO, double2api(0.0)) == 0);
+  assert (api_cmp(API_TWO, int2api(2)) == 0);
+  assert (api_cmp(API_TWO, str2api("2")) == 0);
+  assert (api_cmp(API_TWO, long2api((long) 2)) == 0);
+  assert (api_cmp(API_TWO, double2api(2.0)) == 0);
+  
+  // Larger numbers
+  assert (api_cmp(int2api(large), str2api(large_str)) == 0);
+  assert (api_cmp(int2api(large), long2api((long) large)) == 0);
+  assert (api_cmp(int2api(large), double2api((double) large)) == 0);
+  assert (api_cmp(str2api(large_long_str), long2api(large_long)) == 0);
+  assert (api_cmp(str2api(large_long_str), double2api((double) large_long)) == 0);
+  assert (api_cmp(long2api(large_long), double2api((double) large_long)) == 0);
+
+  // Negative larger numbers
+  assert (api_cmp(int2api(nlarge), str2api(nlarge_str)) == 0);
+  assert (api_cmp(int2api(nlarge), long2api((long) nlarge)) == 0);
+  assert (api_cmp(int2api(nlarge), double2api((double) nlarge)) == 0);
+  assert (api_cmp(str2api(nlarge_long_str), long2api(nlarge_long)) == 0);
+  assert (api_cmp(str2api(nlarge_long_str), double2api((double) nlarge_long)) == 0);
+  assert (api_cmp(long2api(nlarge_long), double2api((double) nlarge_long)) == 0);
+  
+  // And we're done.
+  return 0;
+} // main
