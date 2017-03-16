2f2f1aa674583fbfef865df8669f12749efaa4b4
Alex Mitchell <alex@alexthemitchell.com>
2017-03-15 21:32:26 -0500

    indent

--- Alex/alint.c
+++ Alex/alint.c
@@ -15,7 +15,7 @@ const int E[] = {
   10000000,
   100000000,
   1000000000,
-  };
+};
 
 ALInt *
 ali_add (ALInt * a, ALInt * b)
@@ -23,19 +23,27 @@ ali_add (ALInt * a, ALInt * b)
 
 }
 
-ALInt *ali_subtract (ALInt * a, ALInt * b) {
+ALInt *
+ali_subtract (ALInt * a, ALInt * b)
+{
 
 }
 
-ALInt *ali_multiply (ALInt * a, ALInt * b) {
+ALInt *
+ali_multiply (ALInt * a, ALInt * b)
+{
 
 }
 
-ALInt *ali_quotient (ALInt * a, ALInt * b) {
+ALInt *
+ali_quotient (ALInt * a, ALInt * b)
+{
 
 }
 
-ALInt *ali_remainder (ALInt * a, ALInt * b) {
+ALInt *
+ali_remainder (ALInt * a, ALInt * b)
+{
 
 }
 
@@ -53,72 +61,86 @@ ali_free (ALInt * i)
   free (i);
 }
 
-ALInt *double2ali (double d) {
-  return int2ali((int)d);
+ALInt *
+double2ali (double d)
+{
+  return int2ali ((int) d);
 }
 
-ALInt *long2ali (long l) {
-  return int2ali((int)l);
+ALInt *
+long2ali (long l)
+{
+  return int2ali ((int) l);
 }
 
 ALInt *
 int2ali (int i)
 {
-  char str[15]; // INT_MAX is less than 15 characters wide
-  sprintf(str,"%d",i);
-  return str2ali(str);
+  char str[15];			// INT_MAX is less than 15 characters wide
+  sprintf (str, "%d", i);
+  return str2ali (str);
 }
 
-ALIntDigit *str2ali_k (char *str, ALIntDigit *last, int *length, ALIntDigit **tail) {
-  if (!str[0]) {
-    *tail = last;
-    return NULL;
-  }
-  ALIntDigit *digit = malloc(sizeof(ALIntDigit));
+ALIntDigit *
+str2ali_k (char *str, ALIntDigit * last, int *length, ALIntDigit ** tail)
+{
+  if (!str[0])
+    {
+      *tail = last;
+      return NULL;
+    }
+  ALIntDigit *digit = malloc (sizeof (ALIntDigit));
   (*length)++;
 
   digit->prev = last;
   digit->val = str[0];
-  digit->next = str2ali_k(&str[1], digit, length, tail); 
+  digit->next = str2ali_k (&str[1], digit, length, tail);
 
   return digit;
 }
 
-ALInt *str2ali (char *str) {
-  ALInt *i = malloc(sizeof(ALInt));
+ALInt *
+str2ali (char *str)
+{
+  ALInt *i = malloc (sizeof (ALInt));
   i->length = 0;
-  i->nonnegative = !(str[0] =='-');
+  i->nonnegative = !(str[0] == '-');
   str = i->nonnegative ? str : &str[1];
-  i->head = str2ali_k(str,NULL,&i->length, &i->tail); 
- 
+  i->head = str2ali_k (str, NULL, &i->length, &i->tail);
+
   return i;
 }
 
 long
 ali2long (ALInt * a)
 {
-  return (long)ali2int(a);
+  return (long) ali2int (a);
 }
 
-double ali2double (ALInt * a) {
-  return (double)ali2int(a);
+double
+ali2double (ALInt * a)
+{
+  return (double) ali2int (a);
 }
 
-int ali2int (ALInt * a)
- {
+int
+ali2int (ALInt * a)
+{
   int sum = 0;
   int power = 0;
   ALIntDigit *cur = a->tail;
-  while (sum < INT_MAX && sum >= 0) { // If integer overflow, sum will go negative
-    if (!cur) return (a->nonnegative ? sum : -sum);
-    int val = (int)cur->val - '0';
-    sum += E[power] * val;
+  while (sum < INT_MAX && sum >= 0)
+    {				// If integer overflow, sum will go negative
+      if (!cur)
+	return (a->nonnegative ? sum : -sum);
+      int val = (int) cur->val - '0';
+      sum += E[power] * val;
 
-    power++;
-    cur = cur->prev;
-  }
+      power++;
+      cur = cur->prev;
+    }
   return (a->nonnegative ? INT_MAX : -INT_MAX);
- }
+}
 
 char *
 ali2str (ALInt * a)
@@ -127,10 +149,11 @@ ali2str (ALInt * a)
   char *str = malloc (strlen * sizeof (char));
   ALIntDigit *cur = a->head;
   int i = 0;
-  if (!a->nonnegative) {
-    str[i] = '-'; 
-    i++;
-  }
+  if (!a->nonnegative)
+    {
+      str[i] = '-';
+      i++;
+    }
   while (i < strlen)
     {
       str[i] = cur->val;
--- Alex/test-alint.c
+++ Alex/test-alint.c
@@ -5,90 +5,99 @@
 #include <string.h>
 #include <stdlib.h>
 
-void 
-testNormalInt() {
-  ALInt *i = str2ali("100"); 
-  assert( ali2int(i) == 100 );
-  assert( ali2double(i) == 100.0 );
-  assert( ali2long(i) == 100.0 );
+void
+testNormalInt ()
+{
+  ALInt *i = str2ali ("100");
+  assert (ali2int (i) == 100);
+  assert (ali2double (i) == 100.0);
+  assert (ali2long (i) == 100.0);
 
-  char *str = ali2str(i);
-  assert (strcmp(str,"100") == 0);
+  char *str = ali2str (i);
+  assert (strcmp (str, "100") == 0);
 
-  ali_free(i);
-  free(str);
-}
-
-void testNegativeInt() {
-  ALInt *i = str2ali("-100"); 
-  assert( ali2int(i) == -100 );
-  assert( ali2double(i) == -100.0 );
-  assert( ali2long(i) == -100.0 );
-
-  char *str = ali2str(i);
-  assert (strcmp(str,"-100") == 0);
-
-  ali_free(i);
-  free(str);
-}
-
-void 
-testNormalDouble() {
-  ALInt *i = double2ali(100.0); 
-  assert( ali2int(i) == 100 );
-  assert( ali2double(i) == 100.0 );
-  assert( ali2long(i) == 100.0 );
-
-  char *str = ali2str(i);
-  assert (strcmp(str,"100") == 0);
-
-  ali_free(i);
-  free(str);
-}
-
-void testNegativeDouble() {
-  ALInt *i = double2ali(-100.0); 
-  assert( ali2int(i) == -100 );
-  assert( ali2double(i) == -100.0 );
-  assert( ali2long(i) == -100.0 );
-
-  char *str = ali2str(i);
-  assert (strcmp(str,"-100") == 0);
-
-  ali_free(i);
-  free(str);
-}
-
-void testNegativeLargeInt() {
-  ALInt *i = str2ali("-5256008675309"); 
-  assert( ali2int(i) == -INT_MAX );
-
-  char *str = ali2str(i);
-  assert (strcmp(str,"-5256008675309") == 0);
-
-  ali_free(i);
-  free(str);
+  ali_free (i);
+  free (str);
 }
 
 void
-testLargeInt() {
-  ALInt *i = str2ali("5256008675309"); 
-  assert( ali2int(i) == INT_MAX );
+testNegativeInt ()
+{
+  ALInt *i = str2ali ("-100");
+  assert (ali2int (i) == -100);
+  assert (ali2double (i) == -100.0);
+  assert (ali2long (i) == -100.0);
 
-  char *str = ali2str(i);
-  assert (strcmp(str,"5256008675309") == 0);
+  char *str = ali2str (i);
+  assert (strcmp (str, "-100") == 0);
 
-  ali_free(i);
-  free(str);
+  ali_free (i);
+  free (str);
+}
+
+void
+testNormalDouble ()
+{
+  ALInt *i = double2ali (100.0);
+  assert (ali2int (i) == 100);
+  assert (ali2double (i) == 100.0);
+  assert (ali2long (i) == 100.0);
+
+  char *str = ali2str (i);
+  assert (strcmp (str, "100") == 0);
+
+  ali_free (i);
+  free (str);
+}
+
+void
+testNegativeDouble ()
+{
+  ALInt *i = double2ali (-100.0);
+  assert (ali2int (i) == -100);
+  assert (ali2double (i) == -100.0);
+  assert (ali2long (i) == -100.0);
+
+  char *str = ali2str (i);
+  assert (strcmp (str, "-100") == 0);
+
+  ali_free (i);
+  free (str);
+}
+
+void
+testNegativeLargeInt ()
+{
+  ALInt *i = str2ali ("-5256008675309");
+  assert (ali2int (i) == -INT_MAX);
+
+  char *str = ali2str (i);
+  assert (strcmp (str, "-5256008675309") == 0);
+
+  ali_free (i);
+  free (str);
+}
+
+void
+testLargeInt ()
+{
+  ALInt *i = str2ali ("5256008675309");
+  assert (ali2int (i) == INT_MAX);
+
+  char *str = ali2str (i);
+  assert (strcmp (str, "5256008675309") == 0);
+
+  ali_free (i);
+  free (str);
 }
 
 int
 main (int argc, char *argv[])
 {
-  testNormalInt();
-  testLargeInt();
-  testNegativeInt();
-  testNegativeLargeInt();
-  testNormalDouble();
-  testNegativeDouble();
+  testNormalInt ();
+  testLargeInt ();
+  testNegativeInt ();
+  testNegativeLargeInt ();
+  testNormalDouble ();
+  testNegativeDouble ();
 }
